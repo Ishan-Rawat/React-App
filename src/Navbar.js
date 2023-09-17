0ff510e96120383c8dab157a3c9dce177ba693ff
Ishan-Rawat <rawatishan03@gmail.com>
2023-09-17 10:27:35 +0530

    Added styles, read Style Intro.txt and src/Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,16 @@ const Navbar = () => {
             <h1>The B L O G:</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New blog</a>
+                <a href="/create" style={{
+                    color : "white",
+                    backgroundColor: '#f1356d',
+                    borderRadius: '8px'
+                }}>New blog</a>
+                {/**Normally in HMTL for inline style you would write style="property:value"
+                 * but in JSX the value for the syle attribute can be dynamic, so we wrote style = {}
+                 * Then, the styles are represented as an object (key value pairs) so we wrote something like
+                 * {{key: Value, key: value}}
+                 */}
             </div>
         </nav>
     );

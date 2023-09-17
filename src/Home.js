63c774360980ede5b82a9041001cf6e80aa5a2d5
Ishan-Rawat <rawatishan03@gmail.com>
2023-09-17 14:14:19 +0530

    after using hook

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,23 @@
-const Home = () => {
-    /**In the context of React, state of a component refers to the data in that component at a given time
-     * The data in a component can numbers, strings, booleans, objects etc. 
-     * Lets understanding this by introducing some data to this component. 
-     * Consider a variable name, whos value is changed 
-     */
+import { useState } from 'react';
 
-    let name = 'mario';
-    const clickHandle = () => {
-        name = 'Luigi';
-        console.log(name);
+const Home = () => {
+    //First we specify a value to the hook, and we use array destructuring to get 2 things returned by the Hook
+    //The datatype of the value provided to the hook can be any JS datatype.
+    const [name, setName] = useState('Mario');
+    /**The first value in the array above is the name of the value/variable(?) that the hook returns
+     * The second value in the array is the function that the hook returns that allows us to change the value that we want to monitor
+     * The naming convetion for this function is that we name it set + the name of the variable 
+     */
+    const handleClick = () => {
+        setName('luigi');
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
             <p>{name}</p>
-            <button onClick={clickHandle}>Click to change name</button> 
-            {/**Here we would expect that after clicking the button the name displayed on the page would change.
-             * But it doesn't. On the other hand if we check the console and it logs out the changed name.
-             * Why is this happening?
-             * This is because the variable name is not REACTIVE. Meaning that react is not monitoring it for changes.
-             * For the name to change on the web page, react would have to monitor the variable for changes, and then re-render the JSX template everytime it changes.
-            */}       
-            {/**To make the variable name Reactive we have to use a Hook, called useSate
-             * A hook in react is a special type of function that serves a specific fucntion
-             * Lets see how that is used in the next commit
-             */}
+            <button onClick={handleClick}>Click to change name</button> 
+            
         </div>
      );
 }

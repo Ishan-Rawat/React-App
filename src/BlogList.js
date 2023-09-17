84d368d61c06b788a19b6eea9b58aa7bb79e19d7
Ishan-Rawat <rawatishan03@gmail.com>
2023-09-18 00:16:01 +0530

    basic complete

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,9 @@
-const BlogList = () => {
+const BlogList = (props) => {
+    /**We have to specify the prop that we get as an argument in the Home.js component as a parameter in this component defintion other wise it will still have no clue what 'blogs' means
+     * This function gets the props object as an argument, and the blogs attribute that we specified in this component's tag in Home.js gets added as a property to the props method
+     * So, to get the blogs list we have to access it as a property of the props object as shown below
+     */
+    const blogs = props.blogs;
     return ( 
         <div className="blog-list">
             {blogs.map( (blog) => (

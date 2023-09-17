562472b6af5d8a9684400793b14507aa539569a3
Ishan-Rawat <rawatishan03@gmail.com>
2023-09-17 12:13:34 +0530

    created buttons and event handlers

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,3 @@
-//New navbar component
-// Remember that components are just functions that return a JSX template and then that function is exported at the bottom of the file
 const Navbar = () => {
     return (  
         <nav className="navbar">
@@ -11,11 +9,6 @@ const Navbar = () => {
                     backgroundColor: '#f1356d',
                     borderRadius: '8px'
                 }}>New blog</a>
-                {/**Normally in HMTL for inline style you would write style="property:value"
-                 * but in JSX the value for the syle attribute can be dynamic, so we wrote style = {}
-                 * Then, the styles are represented as an object (key value pairs) so we wrote something like
-                 * {{key: Value, key: value}}
-                 */}
             </div>
         </nav>
     );

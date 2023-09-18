428ff6d93606308b51075efa5c037aa139f5b433
Ishan-Rawat <rawatishan03@gmail.com>
2023-09-19 00:41:42 +0530

    introduction and basics

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,7 +1,11 @@
 const BlogList = ({blogs, title}) => {
-    /**Instead of specifying the properties that we want from the props object, we can destructure it in the parameter defintion itself */
-    // const blogs = props.blogs;
-    // const title = props.title;
+    const handleDelete = (id) => {
+        /**Its not a good idea to put this function here
+         * Its because all our blogs have been initialised in the Home component, they make up the Home component's state.
+         * And we have learnt that to update the state of a component we have to use the set method of the useState hook in that component.
+         * Thus we will have to define the function in the Home component and pass it as a prop to this component.
+         */
+    }
     return ( 
         <div className="blog-list">
             <h2> {title} </h2>
@@ -9,6 +13,14 @@ const BlogList = ({blogs, title}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>delete blog</button>
+                    {
+                        /**
+                         * Now we want to add the functionality of allowing users to delete the blogs
+                         * For that we create a button in the template of the blogs and attach an event handler to it
+                         * And we will wrap the handler function in an anonymous function so that we can use "()" to pass the id of the blog as an argument to it
+                         */
+                    }
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -11,17 +11,6 @@ const Home = () => {
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title={ "All blogs"}/>
-            {
-            /**Now lets see how we can pass different data to the same component and re-use it for a different purpose.
-             * Lets say I want to show only the blogs authored by mario beneath the list of all blogs.
-             * So, we can just reuse the component we created to cycle through a list of blogs, and we can pass it a list of blogs authored by mario and it will do the job.
-             * 
-             * Also, instead writing a new list with just the blogs authored by mario, we can cycle through the list of  alll blogs and just filter out the blogs not written by mario
-             * We can use the filter function for this.
-             * The filter() iterates over a list and we pass it a function as an argument which it calls for each list item. If the function returns true for an item,
-             * then it keeps the item, and if it returns false for an item, it filters the item out.
-             */
-            }
             <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title={"Mario's blogs"}/>
         </div>
      );

2ba4c42a71cdcc3ca618a816e685280dd3e2ad35
Ishan-Rawat <rawatishan03@gmail.com>
2023-09-20 18:05:55 +0530

    intro

--- src/Home.js
+++ src/Home.js
@@ -5,13 +5,8 @@ const Home = () => {
     
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
-    /**At the moment, we are not outputting anything to the page till the time our blogs are fetched. 
-     * Since we are making a request only to our local server the loading is very quick so we dont notice this blank screen.
-     * But now lets create a message that shown on the screen till the blogs load.
-     * For this create an additional piece of state in this component named isPending
-     * isPending is initialised as true and we can use conditional templating to output a template for the loading message while it is true
-     * We can set isPending to false as soon as the blogs load, and it will stop displaying it.
-     */
+    //Right now we are not handling any errors during the fetch process. So in case there is a network error or something the user will just wait indefinitely without any knowledge.
+    //So lets create a catch block that just logs out the error 
     
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
@@ -22,6 +17,7 @@ const Home = () => {
                 setBlogs(data);
                 setIsPending(false);
             })
+            .catch(err => console.log(err))
     },[])
 
     return ( 
